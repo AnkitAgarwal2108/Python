8b5de304f715d81680ed0d3d365fed7f47a2d4f7
ankitagarwal <ankit.jist@rediffmail.com>
2020-10-05 23:06:48 +0530

    oops practice

---
+++ OOPS/oops_8.py
@@ -0,0 +1,15 @@
+#If no __str__ method is defined, print t (or print str(t)) uses __repr__.
+
+class Test:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __repr__(self):
+        return "Test a:%s b:%s" % (self.a, self.b)
+
+    # Driver Code
+
+
+t = Test(1234, 5678)
+print(t)
